edf22bcc68269f54df175eaa4b95ef64644de6e0
Hampus Källberg <hampuskallberg@gmail.com>
2024-12-22 14:26:50 +0100

    pre-clone

--- src/test/java/com/hampus/projektuppgiftapi/PokemonBackendApplicationTests.java
+++ src/test/java/com/hampus/projektuppgiftapi/PokemonBackendApplicationTests.java
@@ -1,8 +1,7 @@
 package com.hampus.projektuppgiftapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjektUppgiftApiApplicationTests {
+class PokemonBackendApplicationTests {
 }
